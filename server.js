ad8f81f18351c6200868bf5a4b0dbe14d680721f
hannahazan <hannahazan2@gmail.com>
2023-11-07 13:43:42 +0100

    change acces cor origin

--- server.js
+++ server.js
@@ -64,20 +64,10 @@ io.on('connection', (socket) => {
 });
 
 var allowedOrigins = ['http://localhost:3000',
-                      'https://tout-permis5-span.vercel.app'];
+                      ];
 app.use(cors(
   {
-    origin: function(origin, callback){
-      // allow requests with no origin 
-      // (like mobile apps or curl requests)
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'The CORS policy for this site does not ' +
-                  'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
+   origin:'https://tout-permis5-span.vercel.app'
   }
 ))
 app.use(express.json());
