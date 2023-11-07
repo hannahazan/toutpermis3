4850a6c6ba012f4edc6c7754b4b5c49783eb9c71
hannahazan <hannahazan2@gmail.com>
2023-11-07 13:08:16 +0100

    change core policy

--- server.js
+++ server.js
@@ -62,7 +62,24 @@ io.on('connection', (socket) => {
     console.log(`${socket.id}: A user connected`);
   })
 });
-app.use(cors())
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://tout-permis5-span.vercel.app'];
+app.use(cors(
+  {
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }
+))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
